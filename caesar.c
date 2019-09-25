11a03b23a5418e57aaa021738d4859f6ed1ac1b7
billlouden <billlouden@users.noreply.github.com>
2019-09-25 16:48:55 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- caesar.c
+++ caesar.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-// Building a Caesar cipher
+// Building a Caesar cipher - second attempt
+// only do what instructor wants
 int main(int argc, string argv[])
     
 {
@@ -12,7 +13,7 @@ int main(int argc, string argv[])
     // must be 2 -- the command + the key
     if (argc != 2)
     {
-        printf("Invalid key\n");
+        // printf("Invalid key\n");
         return 1;
     }
 
@@ -37,23 +38,21 @@ int main(int argc, string argv[])
             // check for uppercase
             if (isupper(plaintext[i]))
             {
-                // get integer at [i] subtract ASCII "A" add k
+                // get integer at [i] subtract "A" add k
                 ch = (((int) plaintext[i] - 65 + k) % 26) + 65;
                 printf("%c", (char) ch);
             }
             else if (islower(plaintext[i]))
             {
-                // get integer at [i] subtract ASCII "a" add k
+                // get integer at [i] subtract "a" add k
                 ch = (((int) plaintext[i] - 97 + k) % 26) + 97;
                 printf("%c", (char) ch);
             }
             else
             {
-                //print other characters as-is
                 printf("%c", plaintext[i]);
             }
-        }
-        //end of cypher, print new line
+        }             
         printf("\n");
         return 0;
     }
