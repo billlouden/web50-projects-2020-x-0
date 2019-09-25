bb81b48ab799f744fb59f054d975cd603dd5b6db
billlouden <billlouden@users.noreply.github.com>
2019-09-25 14:43:40 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- cash.c
+++ cash.c
@@ -1,34 +1,30 @@
 #include <stdio.h>
 #include <math.h>
 #include <cs50.h>
-
+// re-submission following instructions with no
+// extraneous information.
+// compacted code to minimum
 int main()
 {
-    int cents;
+    int cents;    // we will convert dollars into cents
 
     do
     {
+        // get a floating point number
         float dollars = get_float("Change owed: ");
-    if (dollars >999999)
-            printf("I can't count that high\n");
-    else
+        // round to a positive value
         cents = round(dollars * 100);
     }
+
     while (cents <= 0);
-    int dollars = cents / 100;
-    int quarters = (cents % 100 / 25);
+    // how many quarters can be made
+    int quarters = cents / 25;
+    // how many dimes can be made
     int dimes = (cents % 25) / 10;
+    // how many nickels can be made
     int nickels = ((cents % 25) % 10) / 5;
+    // how many pennies can be made
     int pennies = ((cents % 25) % 10) % 5;
-    printf("dollars  :$");
-    printf("%d\n",dollars);
-    printf("quarters :");
-    printf ("%d\n", quarters);
-    printf("dimes    :");
-    printf("%d\n", dimes);
-    printf("nickels  :");
-    printf("%d\n", nickels);
-    printf("pennies  :");
-    printf("%d\n", pennies);           
-    //"+ dimes + nickels + pennies);
+    // print sum
+    printf("%d\n", quarters + dimes + nickels + pennies);
 }
