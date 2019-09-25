ff74417f18a7dda842eb26dfadf61024dd2c70af
billlouden <billlouden@users.noreply.github.com>
2019-09-25 19:26:10 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- caesar.c
+++ caesar.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-// Building a Caesar cipher - second attempt
+// Building a Caesar cipher - 3rd time the charm
 // only do what instructor wants
 int main(int argc, string argv[])
     
@@ -17,44 +17,41 @@ int main(int argc, string argv[])
         return 1;
     }
 
+    // convert string to an integer
+    int k = atoi(argv[1]);
+    // error if integer is <= zero
+    if (k <= 0)
     {
-        // convert string to an integer
-        int k = atoi(argv[1]);
-        // error if integer is zero
-        if (k == 0)
-            
-        {
-            printf("Enter a numeric key\n");
-            return 1;
-        }
-
+        printf("Enter a numeric key\n");
+        return 1;
+    }
+    else
+    {
         // Sucessful key entered
         // Prompt user for message to encrypt
         string plaintext = get_string("Enter your Message\n");
+        printf("ciphertext: ");    
         // iterate through characters up to length of string
         for (int i = 0, n = strlen(plaintext); i < n; i++)
         {
-            int ch = 0;
-            // check for uppercase
-            if (isupper(plaintext[i]))
+        
+            if islower(plaintext[i])            // check for lowercase
+                // get integer at [i] subtract "a" get remainer add k
             {
-                // get integer at [i] subtract "A" add k
-                ch = (((int) plaintext[i] - 65 + k) % 26) + 65;
-                printf("%c", (char) ch);
+                printf("%c", (((plaintext[i] + k) - 97) % 26) + 97);
             }
-            else if (islower(plaintext[i]))
-            {
+            else if (isupper(plaintext[i]))    // check for uppercase
                 // get integer at [i] subtract "a" add k
-                ch = (((int) plaintext[i] - 97 + k) % 26) + 97;
-                printf("%c", (char) ch);
+            {
+                printf("%c", (((plaintext[i] + k) - 65) % 26) + 65);
             }
             else
-            {
+            {            
                 printf("%c", plaintext[i]);
             }
-        }             
-        printf("\n");
-        return 0;
-    }
-} 
-
+                  
+            printf("\n");
+            return 0;
+        }
+    } 
+}
