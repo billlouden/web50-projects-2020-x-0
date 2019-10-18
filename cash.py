469ad53d488ceeaaf824958ab10c519cadcb8957
billlouden <billlouden@users.noreply.github.com>
2019-10-18 19:42:55 +0000

    automated commit by submit50

--- cash.py
+++ cash.py
@@ -1,27 +1,36 @@
+# calculate number of coins to return
+# Bill Louden
+# CS50
+
 import cs50
+
 
 def main():
     while True:
         print("Change owed?")
         amount = cs50.get_float("Change owed: ")
+        # don't allow negative numbers
         if amount >= 0:
             break
-
-    number_of_coins = 0
+    # set coins to 0 and round any fractions
+    coins = 0
     cents = int(round(amount * 100))
-
-    number_of_coins += cents // 25
+    # determine number of quarters then increment coins counter
+    coins += cents // 25
     cents %= 25
 
-    number_of_coins += cents // 10
+    # determine number of dimes from remainder then increment coins counter
+    coins += cents // 10
     cents %= 10
 
-    number_of_coins += cents // 5
+    # determine number of nickles from remainder then increment coins counter
+    coins += cents // 5
     cents %= 5
+    # determine number of pennies from remainder then increment coins counter
+    coins += cents
 
-    number_of_coins += cents
+    print(coins)
 
-    print("{}".format(number_of_coins))
 
 if __name__ == "__main__":
     main()
